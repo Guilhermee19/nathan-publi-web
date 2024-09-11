ecbeffac291a986327526fe45d83f8845979d39d
Guilhermee19 <guilherme@noclaftech.com>
2024-09-11 00:32:08 -0300

    feat: lib AOS animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'nathan-public-web';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // Só inicializa AOS no lado do cliente
+      AOS.init();
+    }
+  }
+
+  // Se precisar atualizar a animação em mudanças dinâmicas, pode usar:
+  reinitializeAOS() {
+    AOS.refresh();
+  }
 }
